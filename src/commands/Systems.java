3b149de46ed2953b5d7c124c0707dbc8ed0e01de
TomRintjema <tomrintjema@hotmail.com>
2015-10-07 16:47:12 -0400

    added systems command

---
+++ src/commands/Systems.java
@@ -0,0 +1,22 @@
+package commands;
+
+import org.jibble.pircbot.PircBot;
+
+public class Systems extends Command {
+
+    public Systems(PircBot bot) {
+        super(bot);
+
+        doesMsgStartWithTrigger = false;
+        trigger = ".systems";
+        name = "Systems";
+        description = "Displays systems of note";
+    }
+
+    public void execute(String channel, String sender, String login, String hostname, String message) {
+        sendMessage(sender, "Home: 63 G. Capricorni");
+        sendMessage(sender, "Fitting: Xihe");
+		sendMessage(sender, "RES: IX Chakan");
+		sendMessage(sender, "Mineing: Frey");
+    }
+}
